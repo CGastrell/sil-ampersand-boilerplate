40f49c1a74c8953d4b58e361313250d374c0c828
Silke Van den Broeck <silkevdb@gmail.com>
2015-08-11 13:43:30 +0200

    view shows name

--- app/views/Hello.js
+++ app/views/Hello.js
@@ -2,6 +2,21 @@ import AmpersandView from 'ampersand-view';
 
 export default AmpersandView.extend({
 
-	template: require('../templates/hello.jade')
+	template: require('../templates/hello.jade'),
+
+	props: {
+		name: 'string'
+	},
+
+	bindings: {
+		name: {
+			type: 'innerHTML',
+			selector: '.name'
+		}
+	},
+
+	initialize: function() {
+		this.name = 'you';
+	}
 
 });
